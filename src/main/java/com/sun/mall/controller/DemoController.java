8003315061c44081f2de72ce92c1be7a7da46e46
zhengdakuangming <729785176@qq.com>
2023-01-06 13:38:29 +0800

    help yang

--- src/main/java/com/sun/mall/controller/DemoController.java
+++ src/main/java/com/sun/mall/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.sun.mall.controller;
 
 import com.sun.mall.service.DemoService;
 import com.sun.mall.utils.R;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,6 @@ public class DemoController {
     @GetMapping("/fetchUserInfo/{username}")
     @ResponseBody
     public R fetchUserInfo(@PathVariable String username)   {
-        return R.ok(demoService.fetchUserInfo(username));
+        return ObjectUtils.isNotEmpty(demoService.fetchUserInfo(username))?R.ok(demoService.fetchUserInfo(username)):R.fail("fail to fetch userInfo");
     }
 }
